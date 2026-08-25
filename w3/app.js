22b797ed4f4e2d6c265d4ec65b2408ec426104fe
Madu <emadu3917@gmail.com>
2026-08-25 22:35:18 +0000

    Step 1 Complete

---
+++ w3/app.js
@@ -0,0 +1,5 @@
+console.log("Week 3 Custom Order Form loaded.");
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed, App is ready for interaction');
+});
